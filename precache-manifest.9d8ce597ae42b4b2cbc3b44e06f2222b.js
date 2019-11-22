75c80e5da65379631fe31e26804ed2d368a841bb
Michael McQuade <giraffesyo@gmail.com>
2019-11-22 11:26:30 -0600

    Updates

---
+++ precache-manifest.9d8ce597ae42b4b2cbc3b44e06f2222b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b81705d41b02673fdc2479084c29e477",
+    "url": "/i-like-you-mao/index.html"
+  },
+  {
+    "revision": "bbc26a0dea32e6ed0f94",
+    "url": "/i-like-you-mao/static/css/main.36acbdfa.chunk.css"
+  },
+  {
+    "revision": "0f258dad0c9a8a21514c",
+    "url": "/i-like-you-mao/static/js/2.53ec098c.chunk.js"
+  },
+  {
+    "revision": "bbc26a0dea32e6ed0f94",
+    "url": "/i-like-you-mao/static/js/main.2e103f93.chunk.js"
+  },
+  {
+    "revision": "25a1c0536e46d36b4e9d",
+    "url": "/i-like-you-mao/static/js/runtime-main.88650eb3.js"
+  }
+]);
