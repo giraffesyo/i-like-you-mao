beab67cba48746092e11d0b08e9abc2a9b28c7c8
Michael McQuade <giraffesyo@gmail.com>
2019-11-22 13:57:45 -0600

    Updates

--- precache-manifest.b182681d8026261889e115e5a669e365.js
+++ precache-manifest.b182681d8026261889e115e5a669e365.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "beea6c896b2fb9b4739302b78f84d3ef",
+    "revision": "394592912dc87d2c34e1f3e7d74e26fc",
     "url": "/index.html"
   },
   {
-    "revision": "e5b568c39c8362265634",
+    "revision": "f0d2cfa547243e03f296",
     "url": "/static/css/main.36acbdfa.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.2950467b.chunk.js"
   },
   {
-    "revision": "e5b568c39c8362265634",
-    "url": "/static/js/main.a55bab41.chunk.js"
+    "revision": "f0d2cfa547243e03f296",
+    "url": "/static/js/main.07a2df03.chunk.js"
   },
   {
     "revision": "1cfd5284de6273a7be26",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9e093c7d78adacd206443d4415a3a602.js"
+  "/precache-manifest.b182681d8026261889e115e5a669e365.js"
 );
 
 self.addEventListener('message', (event) => {
